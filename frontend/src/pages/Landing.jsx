53b4dbf45d5e7a66ec349fb78ebdec4bccfc64a9
anushkaghanekar <anushkaghanekar6@gmail.com>
2026-03-12 14:09:21 +0530

    Remove default files and create structure

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <div>
+      <h2>Welcome to NeuroDerma AI</h2>
+      <p>Upload a skin image to detect possible diseases using AI.</p>
+    </div>
+  );
+}
+
+export default Landing;
